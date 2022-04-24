1b4ccd74f65ee4ad09424d354f3428ebc1f52092
BrunoSBEcker <brunosouza.becker@gmail.com>
2022-04-24 19:13:26 -0300

    implementing routes

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ class Server():
         self.host = host
         self.serverSocket = socket.socket()
         self.serverSocket.bind((host, port))
+        self.routes = {}
+
         print("Socket created")
 
         self.connectionHandler()
@@ -23,15 +25,17 @@ class Server():
             print("\n========\n")
 
             request = self.requestHandler(conn.recv(1024).decode('utf-8').splitlines())
+            self.routerCheck()
 
-            try:
+            
                 file = open('.'+request['headLine'][1],'r')
 
                 responseBody = ''.join([x+'\n' for x in file.readlines()])
                 print(responseBody)
-                respondeHead = "HTTP/1.1 200 ok\n server: pyserver\n content-type: text/html\n connection: close\n\n"
+                respondeHeadLine = "HTTP/1.1 200 ok\n"
+                respondeHeaders = "server: pyserver\n content-type: text/html\n connection: close\n\n"
 
-                conn.send(''.join([respondeHead,responseBody]).encode())
+                conn.send(''.join([respondeHeadLine, respondeHeaders, responseBody]).encode())
                 conn.close()
             except:
                 conn.send('404 not found'.encode())
@@ -52,21 +56,45 @@ class Server():
 
             return {'headLine': requestHeadline, 'headers': requestHeaders, 'body': requestBody}
 
-#soc = socket.socket()
-#
-#soc.bind(('', 8080))
-#soc.listen(5)
-#
-#while True:
-#    conn, addr = soc.accept()
-#
-#    print("Connected with: ",addr)
-#
-#    print(conn.recv(1024).decode('utf-8'))
-#
-#    conn.send("HTTP/1.1 200 ok;\n Connection: close; \nContent-Type: text/html; charset=utf-8; \nServer: Linux; \n\n<h1>Hello World!</h1>".encode())
-#
-#    conn.close()
+    def routerCheck(self, request):
+        pass
+
+    class router():
+
+        def __init__(self):
+            self.routes = {}
+
+        def set_route(self, method, path):
+            self.routes[path] = method
+
+        def routeHandler(self, path, method):
+            if self.routes[path] and self.routes[path] == method:
+                return path
+            else: 
+                return False
+
+"""
+Request 
+        GET /
+          |
+          |
+    Check Routes()
+          |
+        Router
+          |
+          |
+    route handler()
+          |
+          |
+    route exists:
+    |           |
+  True        False
+    |           |
+send Path   send Error
+"""
 
 if __name__ == "__main__": 
-    serverObj = Server(8000, '127.0.0.1')
+    pyServer = Server(8000, '127.0.0.1')
+    router = pyServer.router()
+
+    router.set_route('GET', '/') 
