d91ef62302f843263d0dba7fe73ddd86be15cbbd
BrunoSBecker <54916027+BrunoSBecker@users.noreply.github.com>
2022-04-25 21:21:46 -0300

    Organizing Structure

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ class Server():
     def __init__(self, port, host):
         self.port = port
         self.host = host
+
         self.serverSocket = socket.socket()
         self.serverSocket.bind((host, port))
-        self.routes = {}
-
         print("Socket created")
+
+        self.routes = {'root': '.'}
 
     def start(self):
         self.serverSocket.listen(5)
@@ -22,26 +23,31 @@ class Server():
             print("Connected with {} at port [{}]".format(addr[0], addr[1]))
             print("\n========\n")
 
-            request = self.requestHandler(conn.recv(1024).decode('utf-8').splitlines())
-            try:
-                path = self.routeHandler(request['headLine'][1], request['headLine'][0])
-                file = open('.'+path,'r')
+            data = conn.recv(1024).decode('utf-8').splitlines()
+            request = self.requestHandler(data)
 
-                responseBody = ''.join([x+'\n' for x in file.readlines()])
+            try:
+                path = self.routes[request['headLine'][1]][1]
+                file = open(self.routes['root']+path,'r')
+
                 respondeHeadLine = "HTTP/1.1 200 ok\n"
                 respondeHeaders = "server: pyserver\n content-type: text/html\n connection: close\n\n"
+                responseBody = ''.join([x+'\n' for x in file.readlines()])
 
                 conn.send(''.join([respondeHeadLine, respondeHeaders, responseBody]).encode())
                 conn.close()
             except:
-                conn.send('HTTP/1.1 404 not-found\n server: pyserver\n content-type: text/html\n connection: close\n\n'.encode())
+                conn.send('''HTTP/1.1 404 not-found\n 
+                            server: pyserver\n 
+                            content-type: text/html\n 
+                            connection: close\n\n'''.encode())
                 conn.close()
 
             print(request['headLine'][1])
 
     def requestHandler(self, req):
             request = ''.join((line + '\n') for line in req) 
-
+            print(request)
             requestHead, requestBody = request.split('\n\n', 1)
             requestHead = requestHead.splitlines()
 
@@ -55,34 +61,13 @@ class Server():
     def setRoute(self, method, route, path):
         self.routes[route] = [method, path]
 
-    def routeHandler(self, route, method):
-        if self.routes[route] and self.routes[route][0] == method:
-            return self.routes[route][1]
-        else: 
-            return False
-
-"""
-Request 
-        GET /
-          |
-          |
-    Check Routes()
-          |
-        Router
-          |
-          |
-    route handler()
-          |
-          |
-    route exists:
-    |           |
-  True        False
-    |           |
-send Path   send Error
-"""
+    def setRoot(self, path):
+        self.routes['root'] = path
 
 if __name__ == "__main__": 
-    pyServer = Server(8080, '127.0.0.1')
+    pyServer = Server(8000, '127.0.0.1')
+
+    pyServer.setRoot('./src')
 
     pyServer.setRoute('GET', '/', '/index.html') 
     pyServer.setRoute('GET', '/about-us', '/about.html')
